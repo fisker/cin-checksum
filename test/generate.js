0a0543a72fccc2915bbc00963cc039e469b46b54
fisker <lionkay@gmail.com>
2020-03-23 17:23:09 +0800

    style: update `prettier`

--- test/generate.js
+++ test/generate.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import {generate} from '../src'
 import ids from './_fixtures'
 
-test('should generate right checksum', t => {
+test('should generate right checksum', (t) => {
   for (const id of ids) {
     t.is(generate(id), id[17])
   }
--- test/validate.js
+++ test/validate.js
@@ -2,27 +2,27 @@ import test from 'ava'
 import {validate} from '../src'
 import ids from './_fixtures'
 
-test('should be true', t => {
+test('should be true', (t) => {
   for (const id of ids) {
     t.true(validate(id))
   }
 })
 
-test('should be false', t => {
+test('should be false', (t) => {
   for (const id of ids) {
     const fakeId = `${id.slice(0, 17)}Y`
     t.false(validate(fakeId))
   }
 })
 
-test('should ignore case', t => {
-  for (const id of ids.filter(id => id[17].toLowerCase() === 'x')) {
+test('should ignore case', (t) => {
+  for (const id of ids.filter((id) => id[17].toLowerCase() === 'x')) {
     t.true(validate(id.toLowerCase()))
     t.true(validate(id.toUpperCase()))
   }
 })
 
-test('should accept check bit', t => {
+test('should accept check bit', (t) => {
   const id = ids[0]
   const checksum = id[17]
 
