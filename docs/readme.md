24ddc431d21530b87b236f3e9ae1413f3bc068b7
fisker <lionkay@gmail.com>
2024-07-20 20:07:55 +0800

    Skip argument check for internal calls

---
+++ docs/readme.md
@@ -0,0 +1,3 @@
+# Docs
+
+The PDF version is downloaded from <https://commons.m.wikimedia.org/wiki/File:GB_11643-1999_%E5%85%AC%E6%B0%91%E8%BA%AB%E4%BB%BD%E5%8F%B7%E7%A0%81.pdf>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
-// http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E
+// See /docs/GB_11643-1999_公民身份号码.pdf
 const weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
 const code = '10X98765432'
 
-function generateCinCheckNumber(masterNumber) {
-  if (typeof masterNumber !== 'string' || !/^\d{17}$/.test(masterNumber)) {
-    throw new Error(
-      `The master number should be a 17 digits string, got '${masterNumber}'.`,
-    )
-  }
+const isMasterNumber = (value) =>
+  typeof value !== 'string' || !/^\d{17}$/.test(value)
 
+const isCinNumber = (value) =>
+  // eslint-disable-next-line regexp/use-ignore-case
+  typeof value !== 'string' || /^\d{17}[\dXx]$/.test(value)
+
+function generate(parts) {
   let sum = 0
-  // eslint-disable-next-line unicorn/no-for-loop
-  for (let digit = 0; digit < masterNumber.length; digit += 1) {
-    sum += Number(masterNumber[digit]) * weights[digit]
+  for (let digit = 0; digit < 17; digit += 1) {
+    sum += Number(parts[digit]) * weights[digit]
   }
 
   return code[sum % 11]
 }
 
+function generateCinCheckNumber(masterNumber) {
+  if (!isMasterNumber(masterNumber)) {
+    throw new Error(
+      `The master number should be a 17 digits string, got '${masterNumber}'.`,
+    )
+  }
+
+  return generate(masterNumber)
+}
+
 function isInvalidCinNumber(cinNumber) {
   return !(
-    typeof cinNumber === 'string' &&
-    // eslint-disable-next-line regexp/use-ignore-case
-    /^\d{17}[\dXx]$/.test(cinNumber) &&
-    generateCinCheckNumber(cinNumber.slice(0, 17)) ===
-      cinNumber[17].toUpperCase()
+    isCinNumber(cinNumber) &&
+    generate(cinNumber) === cinNumber[17].toUpperCase()
   )
 }
 
